463d33c940396c156c3ee2571522eb0c1d7525fd
Pedro Neto <pedro.santosnt@hotmail.com>
2020-09-14 11:00:48 -0300

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Pedro Neto on 14/09/20.
+//  Copyright © 2020 Pedro Neto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
